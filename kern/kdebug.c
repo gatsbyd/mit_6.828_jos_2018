a3142c3cdc2fefbf01d873e48bb1ee4ebc96288e
daihaonan <1240184197@qq.com>
2018-10-08 13:23:58 +0800

    cause:lab1 Exercise12(添加栈输出的调试信息) solution:done

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -179,7 +179,13 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 	//	Look at the STABS documentation and <inc/stab.h> to find
 	//	which one.
 	// Your code here.
-
+	stab_binsearch(stabs, &lline, &rline, N_SLINE, addr);
+    if(lline <= rline){
+        info->eip_line = stabs[rline].n_desc;
+    }
+    else {
+        info->eip_line = -1;
+	}
 
 	// Search backwards from the line number for the relevant filename
 	// stab.
