2b71ce1b0b76583c885724ea265c297407505ca9
daihaonan <1240184197@qq.com>
2018-10-31 22:53:55 +0800

    cause:完成Exercise 14, 到目前位置已经支持抢占式CPU轮转 solution:done

--- kern/trap.c
+++ kern/trap.c
@@ -272,6 +272,11 @@ trap_dispatch(struct Trapframe *tf)
 	// Handle clock interrupts. Don't forget to acknowledge the
 	// interrupt using lapic_eoi() before calling the scheduler!
 	// LAB 4: Your code here.
+	if (tf->tf_trapno == IRQ_OFFSET + IRQ_TIMER) {
+		lapic_eoi();
+		sched_yield();
+		return;
+	}
 
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
